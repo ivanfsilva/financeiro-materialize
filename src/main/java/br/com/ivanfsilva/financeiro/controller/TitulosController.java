424cb332e071d6c17565df5e6e0c42a414c70e10
ivanfsilva <ivanfs27@gmail.com>
2020-02-28 15:52:54 -0300

    Configurando layout no projeto e exibindo a página index

---
+++ src/main/java/br/com/ivanfsilva/financeiro/controller/TitulosController.java
@@ -0,0 +1,15 @@
+package br.com.ivanfsilva.financeiro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TitulosController {
+
+    private final String INDEX = "index";
+
+    @RequestMapping
+    public String index() {
+        return INDEX;
+    }
+}
